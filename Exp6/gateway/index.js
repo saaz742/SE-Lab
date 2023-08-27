fb7989af994819a106438cc1f7fd4b362d911f98
Amir Moeini <moeiniamir@outlook.com>
2023-08-28 01:09:31 +0330

    add database

--- Exp6/gateway/index.js
+++ Exp6/gateway/index.js
@@ -2,6 +2,14 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+var redis = require('redis');
+var client = redis.createClient({
+    url: 'redis://:selab6@redis:6379'
+});
+(async () => {
+    await client.connect()
+    console.log('Connected to redis')
+})()
 const app = express();
 const port = 8080
 
@@ -24,6 +32,7 @@ app.post('/api/add/:number1/:number2', async (req, res) => {
         });
         const dataFromBackend1 = response.data;
         console.log(`Backend1 returned ${JSON.stringify(dataFromBackend1)}`);
+        client.set(req.params.number1 + '+' + req.params.number2, dataFromBackend1.result)
         res.json(dataFromBackend1);
     } catch (error) {
         console.error(error);
@@ -43,12 +52,42 @@ app.post('/api/subtract/:number1/:number2', async (req, res) => {
         });
         const dataFromBackend2 = response.data;
         console.log(`Backend2 returned ${JSON.stringify(dataFromBackend2)}`);
+        client.set(req.params.number1 + '-' + req.params.number2, dataFromBackend2.result)
         res.json(dataFromBackend2);
     } catch (error) {
         console.error(error);
         res.status(500).json({error: 'Internal Server Error'});
     }
 })
+
+app.get('/api/history', async (req, res) => {
+    console.log(`Gateway from ${req.ip} history request`)
+    try {
+        json_answer = {}
+        keys = await client.keys('*')
+        for (var i = 0, len = keys.length; i < len; i++) {
+            value = await client.get(keys[i])
+            json_answer[keys[i]] = value
+        }
+        res.json(json_answer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
+app.delete('/api/history', async (req, res) => {
+    console.log(`Gateway from ${req.ip} history delete ${req.query.key}`)
+    try {
+        await client.del(req.query.key)
+        res.json({result: 'success'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+})
+
+
 
 app.listen(port, () => {
     console.log(`Gateway listening at http://localhost:${port}`)
